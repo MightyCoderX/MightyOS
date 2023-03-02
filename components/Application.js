8c050f97cccf4d24ea0132708df761ab3e9a9c94
MightyCoderX <mattiaca003@gmail.com>
2023-03-03 00:46:13 +0100

    Disabled broken code, window focus is temporarily not working FIX

--- components/Application.js
+++ components/Application.js
@@ -16,11 +16,26 @@ class Application
         this.panelIcon.setAttribute('app-desc', this.desc);
         panelApps.appendChild(this.panelIcon);
 
-        this.window = document.createElement('draggable-window');
+        this.window = new DraggableWindow();
         this.window.setAttribute('window-title', this.name);
         this.window.setAttribute('content-url', this.url);
         const iconRect = this.panelIcon.getBoundingClientRect();
         this.window.setAttribute('minimize-origin', `${iconRect.x + iconRect.width/2} ${iconRect.y + iconRect.height/2}`);
+
+        // this.window.addEventListener('shouldfocus', function(e)
+        // {
+        //     Array.from(applications).filter(app => app.window).map(app => app.window).filter(win => win.focused).forEach(w =>
+        //     {
+        //         w.focused = false;
+        //         e.frame.style.zIndex = '0';
+        //     });
+
+        //     this.#focused = true;
+        //     this.#windowFrame.style.zIndex = '1';
+            
+        //     this.#iframe.contentWindow.focus();
+        // });
+
         windowsOverlay.appendChild(this.window);
         
         this.window.addEventListener('close', () =>
